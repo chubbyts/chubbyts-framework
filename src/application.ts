a5ee2324b52ffa4cf629a30ff1a5fa57710c324f
Pedro Bauer <pgm.bauer@gmail.com>
2024-02-05 10:24:56 +0100

    Adapted the createApplication documentation the include a detailed explanation while also expanding on the params and returns.

--- src/application.ts
+++ src/application.ts
@@ -6,6 +6,15 @@ import type { MiddlewareDispatcher } from './middleware/middleware-dispatcher';
 import { createRouteHandler } from './handler/route-handler';
 
 /**
+ * 
+ * Create a web application framework similar to other popular frameworks using ChubbyTS Framework.
+ *  
+ * @param {Array<Middleware>} middlewares - An array of middleware functions to be applied in order.
+ * @param {MiddlewareDispatcher} [middlewareDispatcher] - A custom middleware dispatcher (optional).
+ * @param {Handler} [handler] - A custom route handler (optional).
+ * @returns {Handler} A function that handles incoming HTTP requests.
+ * 
+ * @example
  * ```ts
  * import type { Middleware } from '@chubbyts/chubbyts-http-types/dist/middleware';
  * import { createErrorMiddleware } from '@chubbyts/chubbyts-framework/dist/middleware/error-middleware';
@@ -17,6 +26,9 @@ import { createRouteHandler } from './handler/route-handler';
  *
  * const application = createApplication([ errorMiddleware, routeMatcherMiddleware ]);
  * ```
+ * 
+ * Creates an application that can be further customized to build a simple web application.
+ * The `createApplication` function is a top-level function exported by the ChubbyTS Framework.
  */
 export const createApplication = (
   middlewares: Array<Middleware>,
