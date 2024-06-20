97ce0a7967a529484873d5f1507a28c88620b0a2
Dominik Zogg <dominik.zogg@hotmail.com>
2024-06-20 15:34:40 +0200

    eslint upgrade

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,1 +1,9 @@
-module.exports = require('@chubbyts/chubbyts-eslint/dist/eslintrc').default;
+const config = require('@chubbyts/chubbyts-eslint/dist/eslintrc').default;
+
+module.exports = {
+  ...config,
+  parserOptions: {
+    ...config.parserOptions,
+    project: './tsconfig.eslint.json',
+  },
+};
